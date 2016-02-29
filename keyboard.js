565e698b69174989d5f645457e92427ef800dc2d
Joel Valencia <trekkerj1@gmail.com>
2016-02-28 22:46:40 -0600

    next sentence logic

--- keyboard.js
+++ keyboard.js
@@ -1,11 +1,12 @@
 var main = function() {
-    var letterIndex = 0;
-    var sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
-    var nextLetter = sentences[0].charAt(letterIndex);
-    console.log(nextLetter);
-    
     $('#keyboard-upper-container').hide();
-    $('#words').text(sentences[0]);
+    
+    var sentenceIndex = 0;
+    var sentences = ['one two thre', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+    var letterIndex = 0;
+    var nextLetter = sentences[sentenceIndex].charAt(letterIndex);
+
+    $('#words').text(sentences[sentenceIndex]);
     $('#next-letter').text(nextLetter);
 
     
@@ -26,7 +27,6 @@ var main = function() {
     });
     
     $(document).keypress(function(event){ 
-        console.log(String.fromCharCode(event.which));
         
         if ($("#" + event.which).length) {
             $("#" + event.which).css("background-color", "yellow");
@@ -35,16 +35,24 @@ var main = function() {
         }
         
         var letter = String.fromCharCode(event.which);
-        console.log(letter + "_" + nextLetter);
         
         if (letter == nextLetter) {
             letterIndex++;
-            nextLetter = sentences[0].charAt(letterIndex);
-            $('#next-letter').text(sentences[0].charAt(letterIndex));
+            
+            if (letterIndex == sentences[sentenceIndex].length) {
+                sentenceIndex++;
+                letterIndex = 0;
+                $('#words').text(sentences[sentenceIndex]);
+            }
+            
+            nextLetter = sentences[sentenceIndex].charAt(letterIndex);
+            $('#next-letter').text(sentences[sentenceIndex].charAt(letterIndex));
             
             var letterSpacing = parseInt($('#word-container').css("letter-spacing"), 10);
             $('#block').css("margin-left", "calc(" + letterIndex + "ch + " + (letterSpacing * letterIndex) + "px");
         }
+        
+
     });
     
 }
