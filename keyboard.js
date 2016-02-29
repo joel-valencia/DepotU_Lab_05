fbf43f03a56e40a2ef9785c7a7727de1db0bf790
Joel Valencia <trekkerj1@gmail.com>
2016-02-28 21:00:17 -0600

    first sentence display

--- keyboard.js
+++ keyboard.js
@@ -1,6 +1,9 @@
 var main = function() {
     
+    var sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+    
     $('#keyboard-upper-container').hide();
+    $('#words').text(sentences[0]);
     
     $(document).keydown(function(event){ 
         if (event.which == 16) {
@@ -30,5 +33,7 @@ var main = function() {
     });
     
 }
+
+
 
 $(document).ready(main);
