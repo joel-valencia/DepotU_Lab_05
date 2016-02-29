8431f674d9215f28ea47dbb819f018670b0a2e38
Joel Valencia <trekkerj1@gmail.com>
2016-02-28 22:23:53 -0600

    letter check logic

--- keyboard.js
+++ keyboard.js
@@ -1,23 +1,19 @@
 var main = function() {
     var letterIndex = 0;
-    
     var sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+    var nextLetter = sentences[0].charAt(letterIndex);
+    console.log(nextLetter);
     
     $('#keyboard-upper-container').hide();
     $('#words').text(sentences[0]);
-    $('#next-letter').text(sentences[0].charAt(letterIndex));
+    $('#next-letter').text(nextLetter);
+
     
     $(document).keydown(function(event){ 
         if (event.which == 16) {
             $('#keyboard-lower-container').hide();
             $('#keyboard-upper-container').show();
-            
         }
-
-        letterIndex++;
-        $('#next-letter').text(sentences[0].charAt(letterIndex));
-        var letterSpacing = parseInt($('#word-container').css("letter-spacing"), 10);
-        $('#block').css("margin-left", "calc(" + letterIndex + "ch + " + (letterSpacing * letterIndex) + "px");
     });
     
     $(document).keyup(function(event){ 
@@ -36,6 +32,18 @@ var main = function() {
             $("#" + event.which).css("background-color", "yellow");
         } else {
             $("#" + String.fromCharCode(event.which)).css("background-color", "yellow");
+        }
+        
+        var letter = String.fromCharCode(event.which);
+        console.log(letter + "_" + nextLetter);
+        
+        if (letter == nextLetter) {
+            letterIndex++;
+            nextLetter = sentences[0].charAt(letterIndex);
+            $('#next-letter').text(sentences[0].charAt(letterIndex));
+            
+            var letterSpacing = parseInt($('#word-container').css("letter-spacing"), 10);
+            $('#block').css("margin-left", "calc(" + letterIndex + "ch + " + (letterSpacing * letterIndex) + "px");
         }
     });
     
